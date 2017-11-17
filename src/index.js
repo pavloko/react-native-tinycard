b97d1ddd003d1ea08191d02e65bc61a0264f76e7
pavloko <pavlo.kochubei@hotmail.com>
2017-11-17 13:36:29 +0200

    Initial commit

---
+++ src/index.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import {View, Text} from 'react-native'
+
+const Swipable = () => {
+  return (
+    <View>
+      <Text>This is text. Hello world.</Text>
+    </View>
+  )
+}
+
+export default Swipable
